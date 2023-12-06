63944b1ea79dda6a668f7677b8c3f7c1a3fcea1e
Br0deNN <theo.broden@gmail.com>
2023-12-06 22:12:37 +0100

    Add project files.

---
+++ Cold/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Linq;
+
+namespace Cold
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadLine();
+            string[] nums = Console.ReadLine().Split(' ');
+            int count = 0;
+            foreach (string s in nums)
+            {
+                if (s[0] == '-')
+                {
+                    count++;
+                }
+
+            }
+            Console.WriteLine(count);
+
+
+
+        }
+    }
+}
